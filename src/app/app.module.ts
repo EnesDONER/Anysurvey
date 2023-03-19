abb9ade1a574585fd15cc26abcee01530f261e13
Enes Doner <businessenesdoner@gmail.com>
2023-03-19 22:27:14 +0300

    slider eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NaviComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+const auto = true;
+const intervalTime =5000;
+let slideInterval;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  ngOnInit(): void {
+    if(auto) {
+      slideInterval=setInterval(this.nextSlide,intervalTime)
+    }
+    
+  }
+  
+  nextSlide(){
+    const activeSlide= document.querySelector('.active');
+    activeSlide?.classList.remove('active');
+    if(activeSlide?.nextElementSibling){
+      activeSlide.nextElementSibling.classList.add('active');
+      console.log(activeSlide.nextElementSibling.innerHTML)
+    }else{
+      const slides = document.querySelectorAll('.slide');
+      slides[0].classList.add('active');
+    }
+
+  }
+  prevSlide(){
+    const activeSlide= document.querySelector('.active');
+    activeSlide?.classList.remove('active');
+    if(activeSlide?.previousElementSibling){
+      activeSlide.previousElementSibling.classList.add('active');
+    }else{
+      const slides = document.querySelectorAll('.slide');
+      slides[slides.length-1].classList.add('active');
+    }
+  }
 
 }
+
+
+
